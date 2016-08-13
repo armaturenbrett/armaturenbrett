3db02ccc04d928b3076d43136d2f4e7133606c58
Arik Grahl <mail@arik-grahl.de>
2016-08-13 15:41:22 +0200

    add missing variable

--- app/channels/widget_data_channel.rb
+++ app/channels/widget_data_channel.rb
@@ -1,10 +1,11 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class WidgetDataChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "#{params[:widget]}_widget"
+    widget = params[:widget]
+    stream_from "#{widget}_widget"
 
-    widget_datum = WidgetDatum.find(widget_name)
-    ActionCable.server.broadcast "#{widget_name}_widget",
+    widget_datum = WidgetDatum.find(widget)
+    ActionCable.server.broadcast "#{widget}_widget",
                                  widget_datum.data if widget_datum
   end
 
