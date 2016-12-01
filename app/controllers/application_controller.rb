7c0f298dd965724ab08bfd5c1d0de47983af774d
Robert Greinacher <robert@3antworten.de>
2016-12-01 12:51:15 +0100

    add auto locale selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def dashboard
+  end
+
+  private
+
+  def set_locale
+    @locale = params[:locale]
+    @locale ||= cookies[:locale]
+    @locale ||= request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+
+    I18n.locale = @locale
+    cookies[:locale] = @locale
+
+    @locale
+  rescue
+    I18n.locale = :de
+    @locale = 'de'
+  end
 end
