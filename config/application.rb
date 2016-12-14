02d5ac0e04dcb296c351f8f80c74ea32b2ab2a6b
Arik Grahl <mail@arik-grahl.de>
2016-12-14 19:28:04 +0100

    follow symlinks for locales

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,6 @@ module Dashboard
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
+    config.i18n.load_path += Dir.glob("#{Rails.root.to_s}/config/locales/**{,/*/**}/*.yml")
   end
 end
